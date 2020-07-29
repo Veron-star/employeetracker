f2e18194f98df1d93e22253e58779dde76c94afb
veronica lim <veron_yan@yahoo.com.sg>
2020-07-29 17:02:28 +0800

    created inquirer prompt in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mySql = require("mysql");
 const inquirer = require("inquirer");
 const { start } = require("repl");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 var connection = mySql.createConnection({
     multipleStatements: true,
@@ -16,3 +17,54 @@ connection.connect(function(err) {
     start();
 });
 
+function start() {
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "Please select task: ",
+            name: "task",
+            choices: [
+                "View departments",
+                "View roles",
+                "View employees",
+                "Add department",
+                "Delete department",
+                "Add role",
+                "Delete role",
+                "Add employee",
+                "Delete employee",
+                "Update employee role",
+                "View employees by managers",
+                "Update employee managers",
+                "Escape"   
+            ]
+        }).then(function(answer) {
+            if (answer.action === "View departments") {
+                viewDepartments();
+            } else if (answer.action === "View roles") {
+                viewRoles();
+            } else if (answer.action === "View employees") {
+                viewEmployees();
+            } else if (answer.action === "Add department") {
+                addDepartment();
+            } else if (answer.action === "Delete department") {
+                deleteDepartment();
+            } else if (answer.action === "Add role") {
+                addRole();
+            } else if (answer.action === "Delete role") {
+                deleteRole();
+            } else if (answer.action === "Add employee") {
+                addEmployee();
+            } else if (answer.action === "Delete employee") {
+                deleteEmployee();
+            } else if (answer.action === "Update employee role") {
+                updateEmployeeRole();
+            } else if (answer.action === "View employee by managers") {
+                viewEmployeeByManagers();
+            } else if (answer.action === "Update employee by managers") {
+                updateEmployeeByManagers();
+            } else if (answer.action === "Escape") {
+                connection.end();
+            }
+        });
+};
